0ca23ea11ec048c51ffc978ca52ce2a5439ad639
gregdeli <gregorydel@hotmail.gr>
2024-04-26 11:44:56 +0300

    yes

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,11 +1,10 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 import pandas as pd
 
-# Load the CSV file into a pandas DataFrame
-df = pd.read_csv("Dataset/iphi2802.csv", error_bad_lines=False)
+# Open the dataset
+df = pd.read_csv("Dataset/iphi2802.csv", delimiter="\t")
 
-# Extract the text from the "text" column into a new dataframe
-texts_df = pd.DataFrame(df["text"])
+# Create a dataframe with with the column named "text"
+df_text = df["text"]
 
-# Display the first few rows of the new dataframe
-print(texts_df.head())
+print("stop")
